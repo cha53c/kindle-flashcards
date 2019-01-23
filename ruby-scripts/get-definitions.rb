fca883c727207e122cb7469bdbd7c41409c45163
chrischase <chr15t0pher.cha53@gmail.com>
2019-01-23 20:58:11 +0000

    added settings file to parameterise values

--- ruby-scripts/get-definitions.rb
+++ ruby-scripts/get-definitions.rb
@@ -2,10 +2,28 @@
 
 require 'faraday'
 
+load ('./ruby-scripts/settings.rb')
+
+   # 'Accept': 'application/json',
+   # 'app_id': Meteor.settings.oed.app_id,
+   # 'app_key': Meteor.settings.oed.app_key
+
 begin
 
-  res = Faraday.get 'http://www.google.com'
-  puts res.body
+# http = Faraday.new :url => 'http://api.foo.com',
+# :headers => {
+#   'Accept' => 'application/json,
+#   'app_id' => APP_ID,
+#   'app_key'=> APP_KEY,
+# }
+
+  # res = Faraday.get 'http://www.google.com'
+  # puts res.body
+
+  puts Dir.pwd
+
+  puts 'hello world'
+  puts HELLO
 
 rescue Faraday::ClientError => e
     puts "Exception occurred"
