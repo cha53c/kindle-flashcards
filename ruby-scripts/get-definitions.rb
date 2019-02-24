fd00884f19e89a05a90bb535e49b4ebb52ce7913
chrischase <chr15t0pher.cha53@gmail.com>
2019-02-24 13:00:23 +0000

    cleaned up get-definitions

--- ruby-scripts/get-definitions.rb
+++ ruby-scripts/get-definitions.rb
@@ -5,13 +5,9 @@ require 'faraday'
 
 load ('./ruby-scripts/settings.rb')
 
-   # 'Accept': 'application/json',
-   # 'app_id': Meteor.settings.oed.app_id,
-   # 'app_key': Meteor.settings.oed.app_key
 
 begin
 
-  # http = Faraday.new :url => 'http://google.com'
 http = Faraday.new :url => ROOT_URL,
   :headers => {
   'Accept' => 'application/json',
@@ -22,12 +18,13 @@ http = Faraday.new :url => ROOT_URL,
     # faraday.response :logger
   # end
 
-  file = File.open('./test-files/sample_entry.txt', 'w')
-  # need to build this string for each word in the list
+  # Will need to build this string for each word in the list
   res = http.get '/api/v1/entries/en/ace'
   puts res.body
-  file.puts res.body
 
+  # Used to generate test files. Will need to build this as a feature
+  # file = File.open('./test-files/sample_entry.txt', 'w')
+  # file.puts res.body
 
 rescue Faraday::ClientError => e
     puts "Exception occurred"
