75b5fa07a66e41fb67c0a548b09fe1c94643fa48
chrischase <chr15t0pher.cha53@gmail.com>
2019-03-03 11:24:45 +0000

    refactored tests

--- ruby-scripts/test/parse-entry-test.rb
+++ ruby-scripts/test/parse-entry-test.rb
@@ -6,38 +6,29 @@ require_relative '../parse-entry'
 
 class TestEntry < Test::Unit::TestCase
 
-  def test_entry_returns_hash_of_json
-    file = File.open('./test-files/sample-entry.txt', 'r')
-    entry = file.read
-    d = Entry.new  entry
-    assert_equal(JSON.parse(entry), d.get_entry())
+  def setup
+      file = File.open('./test-files/sample-entry.txt', 'r')
+      text = file.read
+      @entry = Entry.new text
+      file.close()
   end
 
   def test_get_lexicalEntries
-    file = File.open('./test-files/sample-entry.txt', 'r')
-    text = file.read
-    entry = Entry.new text
-    le = entry.get_lexicalEntries()
+    le = @entry.get_lexicalEntries()
     assert_true(le.is_a? Array)
     # assert_equal(3, le.length)
-    puts le
+    # puts le
   end
 
   def test_get_lexicalCategory
-    file = File.open('./test-files/sample-entry.txt', 'r')
-    text = file.read
-    entry = Entry.new text
-    lc = entry.get_lexicalCategory()
+    lc = @entry.get_lexicalCategory()
     assert_true(lc.is_a? Array)
     assert_equal(3, lc.length)
     puts lc
   end
 
   def test_descriptions
-    file = File.open('./test-files/sample-entry.txt', 'r')
-    entry = file.read
-    d = Entry.new  entry
-    definitions = d.get_definitions()
+    definitions = @entry.get_definitions()
     assert_true(definitions.is_a? Array)
     assert_equal(12, definitions.length)
     assert_equal("a person who excels at a particular sport or other activity", definitions[1][0])
