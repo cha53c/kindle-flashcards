3fa94fc1eb511e739da72f7fc78c1341d741fccb
chrischase <chr15t0pher.cha53@gmail.com>
2019-03-11 13:24:50 +0000

    updated text builder

--- ruby-scripts/text-builder.rb
+++ ruby-scripts/text-builder.rb
@@ -11,16 +11,16 @@ class TextBuilder
   def get_card_info()
     entries = @main_entry.get_lexicalEntries()
     # print a description for each lexicalEntries()
+    text = @main_entry.get_word[0] + "\n"
+    text += "----------\n"
     entries.each do |le|
       entry = Entry.new le
-      text = ""
-      text =  entry.get_lexicalCategory[0] + "\n"
       definitions = entry.get_definitions
       text += "1. " + definitions[0] + "\n"
-      text += "2. " + definitions[1] if definitions.length > 1
-      puts text
-      puts "---------------------"
+      text += "2. " + definitions[1] + "\n" if definitions.length > 1
+      text +=  "---------------------\n"
     end
+    puts text
   end
 
 end
