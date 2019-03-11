50eb5cd281bd7fe78f262facdf0eca636931e771
chrischase <chr15t0pher.cha53@gmail.com>
2019-03-11 13:48:26 +0000

    created dictionary lookup and tests

--- ruby-scripts/get-entry.rb
+++ ruby-scripts/get-entry.rb
@@ -5,31 +5,41 @@ require 'faraday'
 
 load ('./ruby-scripts/settings.rb')
 
+class DictionaryLookup
 
-begin
+  def initialize()
+    begin
+      @http = Faraday.new :url => ROOT_URL,
+        :headers => {
+        'Accept' => 'application/json',
+        'app_id' => APP_ID,
+        'app_key' => APP_KEY,
+        }
+        # do |faraday|
+          # faraday.response :logger
+        # end
+    rescue Faraday::ClientError => e
+        puts "Exception occurred"
+        puts e
+    end
+  end
 
-http = Faraday.new :url => ROOT_URL,
-  :headers => {
-  'Accept' => 'application/json',
-  'app_id' => APP_ID,
-  'app_key' => APP_KEY,
-  }
-  # do |faraday|
-    # faraday.response :logger
-  # end
+  def get_entry_for(word)
+      begin
+        # Will need to build this string for each word in the list
+        res = @http.get '/api/v1/entries/en/ace'
+        # puts res.body
+        return res.body
 
-  # Will need to build this string for each word in the list
-  res = http.get '/api/v1/entries/en/ace'
-  puts res.body
+        # Used to generate test files. Will need to build this as a feature
+        # file = File.open('./test-files/sample_entry.txt', 'w')
+        # file.puts res.body
 
-  # Used to generate test files. Will need to build this as a feature
-  # file = File.open('./test-files/sample_entry.txt', 'w')
-  # file.puts res.body
-
-rescue Faraday::ClientError => e
-    puts "Exception occurred"
-    puts e
-
-ensure
-    # db.close if db
+      rescue Faraday::ClientError => e
+        puts "Exception occurred"
+        puts e
+      ensure
+          # db.close if db
+     end
+  end
 end
---
+++ ruby-scripts/test/get_entry_tests.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby
+
+require 'test/unit'
+require 'json'
+require_relative '../get-entry'
+
+class TestEntry < Test::Unit::TestCase
+
+  def setup
+      @dl = DictionaryLookup.new
+  end
+
+  def teardown
+    @dl.close
+  end
+
+  def test_lookup_single_entry
+    @dl.get_entry_for("ace")
+  end
+
+end
