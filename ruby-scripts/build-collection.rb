09caa7df441b958ade04ac0af0a9c26608f6178f
chrischase <chr15t0pher.cha53@gmail.com>
2019-03-12 16:24:11 +0000

    updated to read from db

--- ruby-scripts/build-collection.rb
+++ ruby-scripts/build-collection.rb
@@ -4,10 +4,13 @@ require_relative './get-entry'
 require_relative './file-writer'
 require_relative './parse-entry'
 require_relative './text-builder'
+require_relative './sqlite-script'
 
 
 # TODO: load list of words from database
 words = ["anodyne", "fatuous", "inviolate", "etiolated", "scarcity"]
+vocab_db = DBReader.new
+words = vocab_db.get_word_list
 # TODO: compare with previous list
 csv = CSV.open("./output-files/cards.csv", "wb", {:col_sep => "*"})
 words.each do |w|
