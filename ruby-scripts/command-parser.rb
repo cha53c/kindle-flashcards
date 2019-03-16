9dfbca6e696adc14a3843e5cf3c04cca7e05b9bc
chrischase <chr15t0pher.cha53@gmail.com>
2019-03-16 12:06:58 +0000

    updated to use logger severity

--- ruby-scripts/command-parser.rb
+++ ruby-scripts/command-parser.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/ruby
 
+require 'logger'
 # default ERROR
 # -v  WARNING
 # -vv INFO
 # -vvv DEBUG
 def get_log_level(switches)
-  levels=["ERROR", "WARN", "INFO", "DEBUG"];
+  # levels=["ERROR", "WARN", "INFO", "DEBUG"];
+  levels=[Logger::ERROR, Logger::WARN, Logger::INFO, Logger::DEBUG]
   vs = switches.count "v"
   vs < 4 ? levels[vs] : levels[0]
 
--- ruby-scripts/test/command-parser-tests.rb
+++ ruby-scripts/test/command-parser-tests.rb
@@ -1,26 +1,27 @@
 #!/usr/bin/ruby
 
 require 'test/unit'
+require 'logger'
 require_relative '../command-parser'
 
 class TestEntry < Test::Unit::TestCase
 
 def test_logging_level
-# ["ERROR", "WARN", "INFO", "DEBUG"]
+
   level = get_log_level("")
-  assert_equal("ERROR", level)
+  assert_equal(Logger::ERROR, level)
 
   level = get_log_level("-v")
-  assert_equal("WARN", level)
+  assert_equal(Logger::WARN, level)
 
   level = get_log_level("-vv")
-  assert_equal("INFO", level)
+  assert_equal(Logger::INFO, level)
 
   level = get_log_level("-vvv")
-  assert_equal("DEBUG", level)
+  assert_equal(Logger::DEBUG, level)
 
   level = get_log_level("-vvvv")
-  assert_equal("ERROR", level)
+  assert_equal(Logger::ERROR, level)
 end
 
 end
