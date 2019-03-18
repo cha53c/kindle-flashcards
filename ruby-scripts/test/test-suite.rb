2572f8c456e49e3828be48232b003565d10a6219
chrischase <chr15t0pher.cha53@gmail.com>
2019-03-18 18:04:47 +0000

    added exclude words to test suite

--- ruby-scripts/test/test-suite.rb
+++ ruby-scripts/test/test-suite.rb
@@ -8,6 +8,7 @@ require_relative './command-parser-tests'
 # require_relative './parse-entry-test'
 require_relative './text-builder-tests'
 require_relative './get-entry-tests'
+require_relative './exclude-words-tests'
 
 $LOG = Logger.new(STDOUT)
 $LOG.level = Logger::INFO
