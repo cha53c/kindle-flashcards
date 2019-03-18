d8e0af3766b1005424ad73646e73ab742c4daa28
chrischase <chr15t0pher.cha53@gmail.com>
2019-03-18 18:21:53 +0000

    added exclude list + some clean up

--- ruby-scripts/build-collection.rb
+++ ruby-scripts/build-collection.rb
@@ -8,30 +8,39 @@ require_relative './get-entry'
 require_relative './parse-entry'
 require_relative './text-builder'
 require_relative './sqlite-script'
+require_relative './exclude-words'
 
 $LOG = Logger.new(STDOUT)
 $LOG.level = get_log_level(ARGV[0])
 
 
-# TODO: need to add logging
 # TODO: need to recover when connection is lost.
 #   either circuit breaker or icremental backoff
 # TODO: need to keep track of word the were added already so as not to remake cards
 # TODO: set up test suite to run all test
 # TODO: mock out http calls to test timeouts and lost connections
-$LOG.info("Start Builder started")
-$LOG.info("load database")
+$LOG.info "Start Builder started"
+$LOG.info "load database"
 vocab_db = DBReader.new
-$LOG.info("select words")
+$LOG.info "select words"
 words = vocab_db.get_word_list
-$LOG.debug("word list size #{words.length}")
-# TODO: compare with previous list
+$LOG.debug "word list size #{words.length}"
+
 csv = CSV.open("./output-files/cards.csv", "wb", {:col_sep => "*"})
-$LOG.debug("opened output file ./output-files/cards.csv")
+$LOG.debug "opened output file ./output-files/cards.csv"
+
+$LOG.info "initialize exclude list"
+ew = ExcludeWords.new
+
+$lOG.info "initialize dictionary look up"
+dl = DictionaryLookup.new
 
 words.each do |w|
-  $LOG.info("building card for #{w}")
-  dl = DictionaryLookup.new
+  if ew.exclude? w
+    $LOG.info "#{w} in exclude list"
+    next
+  end
+  $LOG.info "building card for #{w}"
   doc = dl.get_entry_for(w)
   next if doc.nil?
   entry = Entry.new doc
@@ -41,5 +50,5 @@ words.each do |w|
   next if card.nil?
   csv << [w, card]
   sleep 8.5
-  $LOG.info("sleep")
+  $LOG.info "sleep"
 end
